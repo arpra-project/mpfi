e115124c9ef317a3e491be18ed385d86cddc42f0
ptheveny <ptheveny@e2f78e0c-bb60-4709-9413-9660a31d4696>
2009-10-19 17:04:48 +0000

    Add tests for mpfi_abs.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfi@134 e2f78e0c-bb60-4709-9413-9660a31d4696

---
+++ trunk/mpfi/tests/tabs.c
@@ -0,0 +1,44 @@
+/* tabs.c -- Test mpfi_abs.
+
+Copyright 2009
+                     Spaces project, Inria Lorraine
+                     and Salsa project, INRIA Rocquencourt,
+                     and Arenaire project, Inria Rhone-Alpes, France
+                     and Lab. ANO, USTL (Univ. of Lille),  France
+
+
+This file is part of the MPFI Library.
+
+The MPFI Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or (at your
+option) any later version.
+
+The MPFI Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the MPFI Library; see the file COPYING.LIB.  If not, write to
+the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+MA 02110-1301, USA. */
+
+#include "mpfi-tests.h"
+
+int
+main (int argc, char **argv)
+{
+  mpfi_function abs;
+
+  MPFI_SET_FUNCTION (abs, II, mpfi_abs, mpfr_abs);
+
+  test_start ();
+
+  check_data (abs, "abs.dat");
+  check_random (abs, 2, 1000, 10);
+
+  test_end ();
+
+  return 0;
+}
