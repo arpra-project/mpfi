cb1c19da8a4f916b8a422da6743f27240e08874f
ptheveny <ptheveny@e2f78e0c-bb60-4709-9413-9660a31d4696>
2009-10-20 14:00:44 +0000

    Add tests for mpfi_cbrt.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfi@138 e2f78e0c-bb60-4709-9413-9660a31d4696

---
+++ trunk/mpfi/tests/tcbrt.c
@@ -0,0 +1,44 @@
+/* tcbrt.c -- Test mpfi_cbrt.
+
+Copyright 2009
+                     Spaces project, Inria Lorraine
+                     and Salsa project, INRIA Rocquencourt,
+                     and Arenaire project, Inria Rhone-Alpes, France
+                     and Lab. ANO, USTL (Univ. of Lille),  France
+
+
+This file is part of the MPFI Library.
+
+The MPFI Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or (at your
+option) any later version.
+
+The MPFI Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the MPFI Library; see the file COPYING.LIB.  If not, write to
+the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+MA 02110-1301, USA. */
+
+#include "mpfi-tests.h"
+
+int
+main (int argc, char **argv)
+{
+  mpfi_function cbrt;
+
+  MPFI_SET_FUNCTION (cbrt, II, mpfi_cbrt, mpfr_cbrt);
+
+  test_start ();
+
+  check_data (cbrt, "cbrt.dat");
+  check_random (cbrt, 2, 1000, 10);
+
+  test_end ();
+
+  return 0;
+}
