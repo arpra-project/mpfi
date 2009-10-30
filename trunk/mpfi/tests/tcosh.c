76cbe4318765182ea87da02a362cbb3a2ad94fa6
ptheveny <ptheveny@e2f78e0c-bb60-4709-9413-9660a31d4696>
2009-10-30 09:50:15 +0000

    Add tests file (random tests only) for all functions with one interval as input and one interval as output.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfi@146 e2f78e0c-bb60-4709-9413-9660a31d4696

---
+++ trunk/mpfi/tests/tcosh.c
@@ -0,0 +1,44 @@
+/* tcosh.c -- Test mpfi_cosh.
+
+Copyright 2009
+                     Spaces project, Inria Lorraine
+                     and Salsa project, INRIA Rocquencourt,
+                     and Arenaire project, Inria Rhone-Alpes, France
+                     and Lab. ANO, USTL (Univ. of Lille),  France
+
+
+This file is part of the MPFI Library.
+
+The MPFI Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or (at your
+option) any later version.
+
+The MPFI Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the MPFI Library; see the file COPYING.LIB.  If not, write to
+the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+MA 02110-1301, USA. */
+
+#include "mpfi-tests.h"
+
+int
+main (int argc, char **argv)
+{
+  mpfi_function i_cosh;
+
+  MPFI_SET_FUNCTION (i_cosh, II, mpfi_cosh, mpfr_cosh);
+
+  test_start ();
+
+/*   check_data (i_cosh, "cosh.dat"); */
+  check_random (i_cosh, 2, 1000, 10);
+
+  test_end ();
+
+  return 0;
+}
---
+++ trunk/mpfi/tests/tlog2.c
@@ -0,0 +1,44 @@
+/* tlog2.c -- Test mpfi_log2.
+
+Copyright 2009
+                     Spaces project, Inria Lorraine
+                     and Salsa project, INRIA Rocquencourt,
+                     and Arenaire project, Inria Rhone-Alpes, France
+                     and Lab. ANO, USTL (Univ. of Lille),  France
+
+
+This file is part of the MPFI Library.
+
+The MPFI Library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 2.1 of the License, or (at your
+option) any later version.
+
+The MPFI Library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with the MPFI Library; see the file COPYING.LIB.  If not, write to
+the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
+MA 02110-1301, USA. */
+
+#include "mpfi-tests.h"
+
+int
+main (int argc, char **argv)
+{
+  mpfi_function i_log2;
+
+  MPFI_SET_FUNCTION (i_log2, II, mpfi_log2, mpfr_log2);
+
+  test_start ();
+
+/*   check_data (i_log2, "log2.dat"); */
+  check_random (i_log2, 2, 1000, 10);
+
+  test_end ();
+
+  return 0;
+}
