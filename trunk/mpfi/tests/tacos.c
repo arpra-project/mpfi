d9ca70047f723e6fcd514aa9660a3cb531a5cd64
ptheveny <ptheveny@e2f78e0c-bb60-4709-9413-9660a31d4696>
2009-11-19 12:56:48 +0000

    Add test data file for acos.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfi@195 e2f78e0c-bb60-4709-9413-9660a31d4696

--- trunk/mpfi/tests/tacos.c
+++ trunk/mpfi/tests/tacos.c
@@ -35,7 +35,7 @@ main (int argc, char **argv)
 
   test_start ();
 
-/*   check_data (i_acos, "acos.dat"); */
+  check_data (&i_acos, "acos.dat");
   check_random (&i_acos, 2, 1000, 10);
 
   test_end ();
