dc6bb45bd80f5bb7ddd4a53f5e5b9247d4669505
ptheveny <ptheveny@e2f78e0c-bb60-4709-9413-9660a31d4696>
2009-11-25 12:43:59 +0000

    Add test data for mpfi_atan.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfi@202 e2f78e0c-bb60-4709-9413-9660a31d4696

--- trunk/mpfi/tests/tatan.c
+++ trunk/mpfi/tests/tatan.c
@@ -35,7 +35,7 @@ main (int argc, char **argv)
 
   test_start ();
 
-/*   check_data (i_atan, "atan.dat"); */
+  check_data (&i_atan, "atan.dat");
   check_random (&i_atan, 2, 1000, 10);
 
   test_end ();
