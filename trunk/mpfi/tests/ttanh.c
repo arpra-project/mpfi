5774a5e8108873d84eba3876680924527a58a02e
ptheveny <ptheveny@e2f78e0c-bb60-4709-9413-9660a31d4696>
2010-07-13 15:25:41 +0000

    Add test data for mpfi_tanh.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfi@390 e2f78e0c-bb60-4709-9413-9660a31d4696

--- trunk/mpfi/tests/ttanh.c
+++ trunk/mpfi/tests/ttanh.c
@@ -35,7 +35,7 @@ main (int argc, char **argv)
 
   test_start ();
 
-/*   check_data (&i_tanh, "tanh.dat"); */
+  check_data (&i_tanh, "tanh.dat");
   check_random (&i_tanh, 2, 1000, 10);
 
   test_end ();
