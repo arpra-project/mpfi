634abdf99a5995df043cf9106600be04c6311199
ptheveny <ptheveny@e2f78e0c-bb60-4709-9413-9660a31d4696>
2010-07-19 15:13:38 +0000

    Add test data for mpfi_div_si.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfi@427 e2f78e0c-bb60-4709-9413-9660a31d4696

--- trunk/mpfi/tests/tdiv_si.c
+++ trunk/mpfi/tests/tdiv_si.c
@@ -35,7 +35,7 @@ main (int argc, char **argv)
 
   test_start ();
 
-/*   check_data (&i_div_si, "div_si.dat"); */
+  check_data (&i_div_si, "div_si.dat");
   check_random (&i_div_si, 2, 1000, 10);
 
   test_end ();
