9945dde881c94bc2cfe378db5845ce7af322bf95
ptheveny <ptheveny@e2f78e0c-bb60-4709-9413-9660a31d4696>
2010-07-19 10:20:05 +0000

    Check overflow and underflow in tests/tdiv_d.c
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfi@417 e2f78e0c-bb60-4709-9413-9660a31d4696

--- trunk/mpfi/tests/tdiv_d.c
+++ trunk/mpfi/tests/tdiv_d.c
@@ -26,6 +26,96 @@ MA 02110-1301, USA. */
 
 #include "mpfi-tests.h"
 
+void
+check_overflow ()
+{
+  mpfr_t max;
+  mpfi_t a;
+  int inexact;
+
+  mpfi_init2 (a, 53);
+  mpfr_init2 (max, 53);
+  mpfr_set_ui (&(a->left), 1, MPFI_RNDD);
+  mpfr_set_inf (max, +1);
+  mpfr_nextbelow (max);
+  mpfr_set (&(a->right), max, MPFI_RNDU);
+
+  inexact = mpfi_div_d (a, a, 0.5);
+
+  if (!mpfr_inf_p (&(a->right))) {
+    printf ("Error: mpfi_div_d does not correctly handle positive "
+            "overflow.\n");
+    exit (1);
+  }
+
+  if (!MPFI_RIGHT_IS_INEXACT (inexact)) {
+    printf ("Error: mpfi_div_d does not return correct value when positive "
+            "overflow.\n");
+    exit (1);
+  }
+
+  mpfr_set_inf (max, -1);
+  mpfr_nextabove (max);
+  mpfr_set (&(a->left), max, MPFI_RNDD);
+  mpfr_set_ui (&(a->right), 1, MPFI_RNDU);
+
+  inexact = mpfi_div_d (a, a, 0.5);
+
+  if (!mpfr_inf_p (&(a->left))) {
+    printf ("Error: mpfi_div_d does not correctly handle negative "
+            "overflow.\n");
+    exit (1);
+  }
+
+  if (!MPFI_LEFT_IS_INEXACT (inexact)) {
+    printf ("Error: mpfi_div_d does not return correct value when negative "
+            "overflow.\n");
+    exit (1);
+  }
+
+  mpfi_clear (a);
+  mpfr_clear (max);
+}
+
+void
+check_underflow (void)
+{
+  mpfi_t got, expected;
+  double d = 0x1p+100;
+  int inex;
+
+  mpfi_init2 (got, 128);
+  mpfi_init2 (expected, 128);
+
+  mpfi_set_ui (expected, 0);
+  mpfr_nextbelow (&(expected->left));
+  mpfr_nextabove (&(expected->right));
+
+  inex = mpfi_div_d (got, expected, d);
+  if (!MPFI_BOTH_ARE_INEXACT(inex)
+      || !same_mpfr_value (&(got->left), &(expected->left))
+      || !same_mpfr_value (&(got->right), &(expected->right))) {
+    printf ("Error: mpfi_div_d (rop, op, d) does not return correct value\n"
+            "op = ");
+    mpfi_out_str (stdout, 16, 0, expected);
+    printf ("\nd  = %a\ngot      = ", d);
+    mpfi_out_str (stdout, 16, 0, got);
+    printf ("\nexpected = ");
+    mpfi_out_str (stdout, 16, 0, expected);
+    printf ("\n");
+
+    if (!MPFI_BOTH_ARE_INEXACT(inex)) {
+      printf ("return value = %d\nexpected     = %d\n", inex,
+              MPFI_FLAGS_BOTH_ENDPOINTS_INEXACT);
+    }
+
+    exit (1);
+  }
+
+  mpfi_clear (got);
+  mpfi_clear (expected);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -36,6 +126,8 @@ main (int argc, char **argv)
 
   check_data (&i_div_d, "div_d.dat");
   check_random (&i_div_d, 2, 1000, 10);
+  check_overflow ();
+  check_underflow ();
 
   test_end ();
   mpfi_fun_clear (&i_div_d);
