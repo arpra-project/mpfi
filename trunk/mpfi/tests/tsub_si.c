c520ac8c4073cc6e6fd65068387f35c133bb305c
ptheveny <ptheveny@e2f78e0c-bb60-4709-9413-9660a31d4696>
2010-07-20 16:35:27 +0000

    Add test data for mpfi_sub_si.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfi@466 e2f78e0c-bb60-4709-9413-9660a31d4696

--- trunk/mpfi/tests/tsub_si.c
+++ trunk/mpfi/tests/tsub_si.c
@@ -35,7 +35,7 @@ main (int argc, char **argv)
 
   test_start ();
 
-/*   check_data (&i_sub_si, "sub_si.dat"); */
+  check_data (&i_sub_si, "sub_si.dat");
   check_random (&i_sub_si, 2, 1000, 10);
 
   test_end ();
