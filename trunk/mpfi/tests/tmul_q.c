27d30a6e0d77eb97181aacf34509ff08f1a9eb38
ptheveny <ptheveny@e2f78e0c-bb60-4709-9413-9660a31d4696>
2010-07-20 11:55:32 +0000

    Add test data for mpfi_mul_q.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfi@444 e2f78e0c-bb60-4709-9413-9660a31d4696

--- trunk/mpfi/tests/tmul_q.c
+++ trunk/mpfi/tests/tmul_q.c
@@ -26,6 +26,106 @@ MA 02110-1301, USA. */
 
 #include "mpfi-tests.h"
 
+
+void
+check_overflow ()
+{
+  mpfr_t max;
+  mpfi_t a;
+  mpq_t q;
+  int inexact;
+
+  mpq_init (q);
+  mpfi_init2 (a, 53);
+  mpfr_init2 (max, 53);
+  mpq_set_ui (q, 4096, 3);
+  mpfr_set_ui (&(a->left), 1, MPFI_RNDD);
+  mpfr_set_inf (max, +1);
+  mpfr_nextbelow (max);
+  mpfr_set (&(a->right), max, MPFI_RNDU);
+
+  inexact = mpfi_mul_q (a, a, q);
+
+  if (!mpfr_inf_p (&(a->right))) {
+    printf ("Error: mpfi_mul_q does not correctly handle positive "
+            "overflow.\n");
+    exit (1);
+  }
+
+  if (!MPFI_RIGHT_IS_INEXACT (inexact)) {
+    printf ("Error: mpfi_mul_q does not return correct value when positive "
+            "overflow.\n");
+    exit (1);
+  }
+
+  mpfr_set_inf (max, -1);
+  mpfr_nextabove (max);
+  mpfr_set (&(a->left), max, MPFI_RNDD);
+  mpfr_set_ui (&(a->right), 1, MPFI_RNDU);
+
+  inexact = mpfi_mul_q (a, a, q);
+
+  if (!mpfr_inf_p (&(a->left))) {
+    printf ("Error: mpfi_mul_q does not correctly handle negative "
+            "overflow.\n");
+    exit (1);
+  }
+
+  if (!MPFI_LEFT_IS_INEXACT (inexact)) {
+    printf ("Error: mpfi_mul_q does not return correct value when negative "
+            "overflow.\n");
+    exit (1);
+  }
+
+  mpq_clear (q);
+  mpfi_clear (a);
+  mpfr_clear (max);
+}
+
+void
+check_underflow (void)
+{
+  mpfi_t got, expected;
+  mpq_t q;
+  int inex;
+
+  mpq_init (q);
+  mpfi_init2 (got, 128);
+  mpfi_init2 (expected, 128);
+
+  mpq_set_ui (q, 1, 1024);
+  mpfi_set_ui (expected, 0);
+  mpfr_nextbelow (&(expected->left));
+  mpfr_nextabove (&(expected->right));
+
+  inex = mpfi_mul_q (got, expected, q);
+  if (!MPFI_BOTH_ARE_INEXACT(inex)
+      || !same_mpfr_value (&(got->left), &(expected->left))
+      || !same_mpfr_value (&(got->right), &(expected->right))) {
+    printf ("Error: mpfi_mul_q (rop, op, q) does not return correct value\n"
+            "op = ");
+    mpfi_out_str (stdout, 16, 0, expected);
+    printf ("\nq  = ");
+    mpq_out_str (stdout, 16, q);
+    printf ("\ngot      = ");
+    mpfi_out_str (stdout, 16, 0, got);
+    printf ("\nexpected = ");
+    mpfi_out_str (stdout, 16, 0, expected);
+    printf ("\n");
+
+    if (!MPFI_BOTH_ARE_INEXACT(inex)) {
+      printf ("return value = %d\nexpected     = %d\n", inex,
+              MPFI_FLAGS_BOTH_ENDPOINTS_INEXACT);
+    }
+
+    exit (1);
+  }
+
+  mpq_clear (q);
+  mpfi_clear (got);
+  mpfi_clear (expected);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -34,8 +134,10 @@ main (int argc, char **argv)
   mpfi_fun_init_IIQ (&i_mul_q, mpfi_mul_q, mpfr_mul_q);
   test_start ();
 
-/*   check_data (&i_mul_q, "mul_q.dat"); */
+  check_data (&i_mul_q, "mul_q.dat");
   check_random (&i_mul_q, 2, 1000, 10);
+  check_overflow ();
+  check_underflow ();
 
   test_end ();
   mpfi_fun_clear (&i_mul_q);
