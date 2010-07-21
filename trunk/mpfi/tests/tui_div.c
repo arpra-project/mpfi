7cd5fa18fa20c68af1e332e4f99c7858e88cb11d
ptheveny <ptheveny@e2f78e0c-bb60-4709-9413-9660a31d4696>
2010-07-21 14:33:42 +0000

    Add test data for mpfi_ui_div.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfi@492 e2f78e0c-bb60-4709-9413-9660a31d4696

--- trunk/mpfi/tests/tui_div.c
+++ trunk/mpfi/tests/tui_div.c
@@ -25,6 +25,54 @@ the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 MA 02110-1301, USA. */
 
 #include "mpfi-tests.h"
+void
+check_overflow (void)
+{
+  mpfi_t interval;
+  mpfi_t got;
+  unsigned long ui = 1024.0;
+  int inex;
+
+  mpfi_init2 (interval, 53);
+  mpfi_init2 (got, 53);
+
+  /* right overflow: 1024 / [epsilon, 1] = [1024, +oo] */
+  mpfr_set_ui (&(interval->left), 0, MPFI_RNDD);
+  mpfr_nextabove (&(interval->left)); /* tiny left endpoint x0 */
+  mpfr_set_ui (&(interval->right), 1, MPFI_RNDU);
+
+  inex = mpfi_ui_div (got, ui, interval);
+  if (MPFI_LEFT_IS_INEXACT (inex) || mpfr_cmp_ui (&(got->left), ui) != 0
+      || !MPFI_RIGHT_IS_INEXACT (inex) || !mpfr_inf_p (&(got->right))) {
+    printf ("Error: mpfi_ui_div (rop, %lu, op) does not correctly handle "
+            "overflow.\nop = ", ui);
+    mpfi_out_str (stdout, 10, 0, interval);
+    printf ("\nrop = ");
+    mpfi_out_str (stdout, 10, 0, got);
+    printf ("\nreturn value = %d\n", inex);
+    exit (1);
+  }
+
+  /* left overflow: 1024.0 / [-1, -epsilon] = [-oo, -1024] */
+  mpfi_neg (interval, interval);
+
+  inex = mpfi_ui_div (got, ui, interval);
+  if (!MPFI_LEFT_IS_INEXACT (inex)
+      || !mpfr_inf_p (&(got->left))
+      || MPFI_RIGHT_IS_INEXACT (inex)
+      || mpfr_cmp_si (&(got->right), -(long)ui) != 0) {
+    printf ("Error: mpfi_ui_div (rop, %lu, op) does not correctly handle "
+            "overflow.\nop = ", ui);
+    mpfi_out_str (stdout, 10, 0, interval);
+    printf ("\nrop = ");
+    mpfi_out_str (stdout, 10, 0, got);
+    printf ("\nreturn value = %d\n", inex);
+    exit (1);
+  }
+
+  mpfi_clear (interval);
+  mpfi_clear (got);
+}
 
 int
 main (int argc, char **argv)
@@ -35,8 +83,9 @@ main (int argc, char **argv)
 
   test_start ();
 
-/*   check_data (&i_ui_div, "ui_div.dat"); */
+  check_data (&i_ui_div, "ui_div.dat");
   check_random (&i_ui_div, 2, 1000, 10);
+  check_overflow ();
 
   test_end ();
   mpfi_fun_clear (&i_ui_div);
