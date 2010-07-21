32863799dc4e99b9ab1a712fadb0f7aa18c85209
ptheveny <ptheveny@e2f78e0c-bb60-4709-9413-9660a31d4696>
2010-07-21 15:01:23 +0000

    Add test data for mpfi_z_div.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfi@497 e2f78e0c-bb60-4709-9413-9660a31d4696

--- trunk/mpfi/tests/tz_div.c
+++ trunk/mpfi/tests/tz_div.c
@@ -26,8 +26,64 @@ MA 02110-1301, USA. */
 
 #include "mpfi-tests.h"
 
+void
+check_overflow (void)
+{
+  mpfi_t interval;
+  mpfi_t got;
+  mpz_t z;
+  int inex;
+
+  mpz_init (z);
+  mpfi_init2 (interval, 53);
+  mpfi_init2 (got, 53);
+
+  /* right overflow: 1024 / [epsilon, 1] = [1024, +oo] */
+  mpz_set_ui (z, 1024);
+  mpfr_set_ui (&(interval->left), 0, MPFI_RNDD);
+  mpfr_nextabove (&(interval->left)); /* tiny left endpoint x0 */
+  mpfr_set_ui (&(interval->right), 1, MPFI_RNDU);
+
+  inex = mpfi_z_div (got, z, interval);
+  if (MPFI_LEFT_IS_INEXACT (inex) || mpfr_cmp_z (&(got->left), z) != 0
+      || !MPFI_RIGHT_IS_INEXACT (inex) || !mpfr_inf_p (&(got->right))) {
+    printf ("Error: mpfi_z_div (rop, z, op) does not correctly handle "
+            "overflow.\n  z = ");
+    mpz_out_str (stdout, 10, z);
+    printf ("\n op = ");
+    mpfi_out_str (stdout, 10, 0, interval);
+    printf ("\nrop = ");
+    mpfi_out_str (stdout, 10, 0, got);
+    printf ("\nreturn value = %d\n", inex);
+    exit (1);
+  }
+
+  /* left overflow: 1024.0 / [-1, -epsilon] = [-oo, -1024] */
+  mpfi_neg (interval, interval);
+
+  inex = mpfi_z_div (got, z, interval);
+  if (!MPFI_LEFT_IS_INEXACT (inex)
+      || !mpfr_inf_p (&(got->left))
+      || MPFI_RIGHT_IS_INEXACT (inex)
+      || mpfr_cmp_si (&(got->right), -1024) != 0) {
+    printf ("Error: mpfi_z_div (rop, z, op) does not correctly handle "
+            "overflow.\n  z = ");
+    mpz_out_str (stdout, 10, z);
+    printf ("\n op = ");
+    mpfi_out_str (stdout, 10, 0, interval);
+    printf ("\nrop = ");
+    mpfi_out_str (stdout, 10, 0, got);
+    printf ("\nreturn value = %d\n", inex);
+    exit (1);
+  }
+
+  mpz_clear (z);
+  mpfi_clear (interval);
+  mpfi_clear (got);
+}
+
 /* fake non-existing function */
-static int
+int
 mpfr_z_div (mpfr_ptr x, mpz_srcptr z, mpfr_srcptr y, mp_rnd_t rnd)
 {
   int ret;
@@ -49,8 +105,9 @@ main (int argc, char **argv)
   mpfi_fun_init_IZI (&i_z_div, mpfi_z_div, mpfr_z_div);
   test_start ();
 
-/*   check_data (&i_z_div, "z_div.dat"); */
+  check_data (&i_z_div, "z_div.dat");
   check_random (&i_z_div, 2, 1000, 10);
+  check_overflow ();
 
   test_end ();
   mpfi_fun_clear (&i_z_div);
