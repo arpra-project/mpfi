b103367afde81154032176a531ff90af43753d69
ptheveny <ptheveny@e2f78e0c-bb60-4709-9413-9660a31d4696>
2010-07-21 15:09:11 +0000

    Add test data for mpfi_z_sub.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfi@500 e2f78e0c-bb60-4709-9413-9660a31d4696

--- trunk/mpfi/tests/tz_sub.c
+++ trunk/mpfi/tests/tz_sub.c
@@ -26,8 +26,67 @@ MA 02110-1301, USA. */
 
 #include "mpfi-tests.h"
 
+void
+check_overflow ()
+{
+  mpfi_t interval;
+  mpfi_t got;
+  mpz_t z;
+  int inex;
+
+  mpfi_init2 (interval, 53);
+  mpfi_init2 (got, 53);
+  mpz_init (z);
+
+  /* right overflow: 1023 - [-Max, 0] = [1023, +oo] */
+  mpz_set_ui (z, 1023);
+  mpfr_set_inf (&(interval->left), -1);
+  mpfr_nextabove (&(interval->left));
+  mpfr_set_ui (&(interval->right), 0, MPFI_RNDU);
+
+  inex = mpfi_z_sub (got, z, interval);
+
+  if (MPFI_LEFT_IS_INEXACT (inex) || mpfr_cmp_z (&(got->left), z) != 0
+      || !MPFI_RIGHT_IS_INEXACT (inex) || !mpfr_inf_p (&(got->right))) {
+    printf ("Error: mpfi_fr_sub (rop, z, op) does not correctly handle "
+            "overflow.\n  z = ");
+    mpz_out_str (stdout, 16, z);
+    printf ("\nop = ");
+    mpfi_out_str (stdout, 16, 0, interval);
+    printf ("\nrop = ");
+    mpfi_out_str (stdout, 16, 0, got);
+    printf ("\nreturn value = %d\n", inex);
+    exit (1);
+  }
+
+  /* left overflow: -1023 - [0, Max] = [-oo, -1023] */
+  mpz_neg (z, z);
+  mpfi_neg (interval, interval);
+
+  inex = mpfi_z_sub (got, z, interval);
+
+  if (!MPFI_LEFT_IS_INEXACT (inex)
+      || !mpfr_inf_p (&(got->left))
+      || MPFI_RIGHT_IS_INEXACT (inex)
+      || mpfr_cmp_si (&(got->right), -1023) != 0) {
+    printf ("Error: mpfi_fr_sub (rop, z, op) does not correctly handle "
+            "overflow.\n  z = ");
+    mpz_out_str (stdout, 16, z);
+    printf ("\nop = ");
+    mpfi_out_str (stdout, 16, 0, interval);
+    printf ("\nrop = ");
+    mpfi_out_str (stdout, 16, 0, got);
+    printf ("\nreturn value = %d\n", inex);
+    exit (1);
+  }
+
+  mpz_clear (z);
+  mpfi_clear (interval);
+  mpfi_clear (got);
+}
+
 /* fake non-existing function */
-static int
+int
 mpfr_z_sub (mpfr_ptr x, mpz_srcptr z, mpfr_srcptr y, mp_rnd_t rnd)
 {
   int ret;
@@ -50,8 +109,9 @@ main (int argc, char **argv)
   mpfi_fun_init_IZI (&i_z_sub, mpfi_z_sub, mpfr_z_sub);
   test_start ();
 
-/*   check_data (&i_z_sub, "z_sub.dat"); */
+  check_data (&i_z_sub, "z_sub.dat");
   check_random (&i_z_sub, 2, 1000, 10);
+  check_overflow ();
 
   test_end ();
   mpfi_fun_clear (&i_z_sub);
