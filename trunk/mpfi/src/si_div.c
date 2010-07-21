424564b3311528b9fdb7c482b0df71149a2f22be
ptheveny <ptheveny@e2f78e0c-bb60-4709-9413-9660a31d4696>
2010-07-21 13:47:33 +0000

    Fix bug in mpfi_si_div: a wrong inexact flag could be returned when the output interval has one (and only one) infinite or zero endpoint.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfi@485 e2f78e0c-bb60-4709-9413-9660a31d4696

--- trunk/mpfi/src/si_div.c
+++ trunk/mpfi/src/si_div.c
@@ -1,6 +1,6 @@
 /* si_div.c -- Divide a signed long int by an interval.
 
-Copyright 1999, 2000, 2001, 2002, 2003, 2004, 2005,
+Copyright 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2010,
                      Spaces project, Inria Lorraine
                      and Salsa project, INRIA Rocquencourt,
                      and Arenaire project, Inria Rhone-Alpes, France
@@ -24,7 +24,6 @@ the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 MA 02110-1301, USA. */
 
 
-#include <stdio.h>
 #include "mpfi.h"
 #include "mpfi-impl.h"
 
@@ -32,26 +31,18 @@ int
 mpfi_si_div (mpfi_ptr a, const long b, mpfi_srcptr c)
 {
   mpfi_t tmp;
-  int inexact_div, inexact = 0;
+  int inexact;
   mp_prec_t tmpprec;
 
   tmpprec = mpfi_get_prec (a);
   if (sizeof(b) * 8 > tmpprec) tmpprec = sizeof(b) * 8;
   mpfi_init2 (tmp, tmpprec);
   mpfi_set_si (tmp, b); /* Exact */
-  inexact_div = mpfi_div (a, tmp, c);
+  inexact = mpfi_div (a, tmp, c);
   MPFI_CLEAR (tmp);
 
   if (MPFI_NAN_P (a))
     MPFR_RET_NAN;
-
-  if (mpfr_inf_p (&(a->left))  && MPFI_LEFT_IS_INEXACT (inexact_div))   /* overflow */
-    inexact += 1;
-  if (mpfr_inf_p (&(a->right)) && MPFI_RIGHT_IS_INEXACT (inexact_div))  /* overflow */
-    inexact += 2;
-  if (mpfi_bounded_p (a)) {
-    inexact = inexact_div;
-  }
 
   return inexact;
 }
