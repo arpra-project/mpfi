17af39ac5ae8284dc41d63facd7cd7b0c2660df5
ptheveny <ptheveny@e2f78e0c-bb60-4709-9413-9660a31d4696>
2010-07-21 11:18:44 +0000

    Add test data for mpfi_fr_sub.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfi@479 e2f78e0c-bb60-4709-9413-9660a31d4696

--- trunk/mpfi/tests/tfr_sub.c
+++ trunk/mpfi/tests/tfr_sub.c
@@ -26,6 +26,65 @@ MA 02110-1301, USA. */
 
 #include "mpfi-tests.h"
 
+void
+check_overflow (void)
+{
+  mpfi_t interval;
+  mpfi_t got;
+  mpfr_t x;
+  int inex;
+
+  mpfi_init2 (interval, 53);
+  mpfi_init2 (got, 53);
+  mpfr_init2 (x, 53);
+
+  /* right overflow: 1023 - [-Max, 0] = [1023, +oo] */
+  mpfr_set_ui (x, 1023, MPFI_RNDD);
+  mpfr_set_inf (&(interval->left), -1);
+  mpfr_nextabove (&(interval->left));
+  mpfr_set_ui (&(interval->right), 0, MPFI_RNDU);
+
+  inex = mpfi_fr_sub (got, x, interval);
+
+  if (MPFI_LEFT_IS_INEXACT (inex) || mpfr_cmp (&(got->left), x) != 0
+      || !MPFI_RIGHT_IS_INEXACT (inex) || !mpfr_inf_p (&(got->right))) {
+    printf ("Error: mpfi_fr_sub (rop, x, op) does not correctly handle "
+            "overflow.\n  x = ");
+    mpfr_out_str (stdout, 16, 0, x, MPFI_RNDD);
+    printf ("\nop = ");
+    mpfi_out_str (stdout, 16, 0, interval);
+    printf ("\nrop = ");
+    mpfi_out_str (stdout, 16, 0, got);
+    printf ("\nreturn value = %d\n", inex);
+    exit (1);
+  }
+
+  /* left overflow: -1023 - [0, Max] = [-oo, -1023] */
+  mpfr_neg (x, x, MPFI_RNDD);
+  mpfi_neg (interval, interval);
+
+  inex = mpfi_fr_sub (got, x, interval);
+
+  if (!MPFI_LEFT_IS_INEXACT (inex)
+      || !mpfr_inf_p (&(got->left))
+      || MPFI_RIGHT_IS_INEXACT (inex)
+      || mpfr_cmp_si (&(got->right), -1023) != 0) {
+    printf ("Error: mpfi_fr_sub (rop, x, op) does not correctly handle "
+            "overflow.\n  x = ");
+    mpfr_out_str (stdout, 16, 0, x, MPFI_RNDD);
+    printf ("\nop = ");
+    mpfi_out_str (stdout, 16, 0, interval);
+    printf ("\nrop = ");
+    mpfi_out_str (stdout, 16, 0, got);
+    printf ("\nreturn value = %d\n", inex);
+    exit (1);
+  }
+
+  mpfr_clear (x);
+  mpfi_clear (interval);
+  mpfi_clear (got);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -34,8 +93,9 @@ main (int argc, char **argv)
   mpfi_fun_init_IRI (&i_fr_sub, mpfi_fr_sub, mpfr_sub);
   test_start ();
 
-/*   check_data (&i_fr_sub, "fr_sub.dat"); */
+  check_data (&i_fr_sub, "fr_sub.dat");
   check_random (&i_fr_sub, 2, 1000, 10);
+  check_overflow ();
 
   test_end ();
   mpfi_fun_clear (&i_fr_sub);
