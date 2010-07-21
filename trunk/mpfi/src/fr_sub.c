a8740fdbea8e061bea2b468d03e735be33f4270e
ptheveny <ptheveny@e2f78e0c-bb60-4709-9413-9660a31d4696>
2010-07-21 11:17:25 +0000

    Simplify code in mpfi_fr_sub using the right precision for temporary variable.
    git-svn-id: svn://scm.gforge.inria.fr/svnroot/mpfi@477 e2f78e0c-bb60-4709-9413-9660a31d4696

--- trunk/mpfi/src/fr_sub.c
+++ trunk/mpfi/src/fr_sub.c
@@ -1,6 +1,6 @@
 /* fr_sub.c -- Subtract an interval from a floating-point number.
 
-Copyright 1999, 2000, 2001, 2002, 2003, 2004, 2005,
+Copyright 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2010
                      Spaces project, Inria Lorraine
                      and Salsa project, INRIA Rocquencourt,
                      and Arenaire project, Inria Rhone-Alpes, France
@@ -24,7 +24,6 @@ the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston,
 MA 02110-1301, USA. */
 
 
-#include <stdio.h>
 #include "mpfi.h"
 #include "mpfi-impl.h"
 
@@ -32,28 +31,15 @@ int
 mpfi_fr_sub (mpfi_ptr a, mpfr_srcptr b, mpfi_srcptr c)
 {
   mpfi_t tmp;
-  int inexact_set, inexact_sub, inexact=0;
+  int inexact;
 
-  mpfi_init2 (tmp, mpfi_get_prec (a));
-  inexact_set = mpfi_set_fr (tmp, b);
-  inexact_sub = mpfi_sub (a, tmp, c);
+  mpfi_init2 (tmp, mpfr_get_prec (b));
+  mpfi_set_fr (tmp, b); /* exact */
+  inexact = mpfi_sub (a, tmp, c);
   MPFI_CLEAR (tmp);
 
   if (MPFI_NAN_P (a))
     MPFR_RET_NAN;
-
-  if ( mpfr_inf_p (&(a->left)) ) {
-    if  (MPFI_LEFT_IS_INEXACT (inexact_sub)) /* overflow */
-      inexact += 1;
-  }
-  else if (MPFI_LEFT_IS_INEXACT (inexact_set) || MPFI_LEFT_IS_INEXACT (inexact_sub))
-    inexact += 1;
-  if ( mpfr_inf_p (&(a->right)) ) {
-    if (MPFI_RIGHT_IS_INEXACT (inexact_sub) )  /* overflow */
-      inexact += 2;
-  }
-  else if (MPFI_RIGHT_IS_INEXACT (inexact_set) || MPFI_RIGHT_IS_INEXACT (inexact_sub))
-    inexact += 2;
 
   return inexact;
 }
